49f33cffd6d2581e841de1b49c2d7099f82cec2b
Maxim Nekrasov <kgtu@mail.ru>
2021-02-08 10:54:49 -0500

    Initial commit

---
+++ desktop/src/com/kgtu/spacewars/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kgtu.spacewars.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kgtu.spacewars.SpaceWarsGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceWarsGame(), config);
+	}
+}
