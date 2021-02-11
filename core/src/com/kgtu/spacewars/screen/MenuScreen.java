06b4ffe8acf93d88ae089494e076d4e7f91c824d
Maxim Nekrasov <kgtu@mail.ru>
2021-02-11 10:47:46 -0500

    hw1 done

---
+++ core/src/com/kgtu/spacewars/screen/MenuScreen.java
@@ -0,0 +1,65 @@
+package com.kgtu.spacewars.screen;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
+
+import com.kgtu.spacewars.base.BaseScreen;
+
+public class MenuScreen extends BaseScreen {
+
+    private Texture img;
+    private Vector2 touch;
+    private Vector2 v;
+
+    @Override
+    public void show() {
+        super.show();
+        img = new Texture("badlogic.jpg");
+        touch = new Vector2();
+        v = new Vector2(1, 1);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0.3f, 0.6f, 0.4f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        batch.begin();
+        batch.draw(img, touch.x, touch.y);
+        batch.end();
+        touch.add(v);
+    }
+
+    @Override
+    public void dispose() {
+        img.dispose();
+        super.dispose();
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        return false;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        switch (keycode) {
+            case Input.Keys.UP:
+                touch.y += 10;
+                break;
+            case Input.Keys.DOWN:
+                touch.y -= 10;
+                break;
+            case Input.Keys.RIGHT:
+                touch.x += 10;
+                break;
+            case Input.Keys.LEFT:
+                touch.x -= 10;
+                break;
+        }
+        return false;
+    }
+}
