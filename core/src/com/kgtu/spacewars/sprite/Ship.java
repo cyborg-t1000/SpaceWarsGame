8b78250055cd2b3343a3a7b63ffa055bab2bcb63
Maxim Nekrasov <kgtu@mail.ru>
2021-02-18 15:23:48 -0500

    Optimize

--- core/src/com/kgtu/spacewars/sprite/Ship.java
+++ core/src/com/kgtu/spacewars/sprite/Ship.java
@@ -1,11 +1,15 @@
 package com.kgtu.spacewars.sprite;
 
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.kgtu.spacewars.base.Sprite;
+import com.kgtu.spacewars.math.Rnd;
 
 public class Ship extends Sprite {
-    public Ship(TextureRegion region) {
-        super(region);
+
+    public Ship(TextureAtlas atlas) {
+        super(atlas.findRegion("main_ship"));
+        regions[0].setRegionWidth(regions[0].getRegionWidth()/2);
+        setHeightProportion(0.1f);
     }
 }
 
