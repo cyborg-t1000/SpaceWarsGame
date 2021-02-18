7210ecf8e6eff1c1845e238444cc37a9ad87d6a3
Maxim Nekrasov <kgtu@mail.ru>
2021-02-18 10:56:46 -0500

    Optimize

---
+++ core/src/com/kgtu/spacewars/sprite/Ship.java
@@ -0,0 +1,12 @@
+package com.kgtu.spacewars.sprite;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.kgtu.spacewars.base.Sprite;
+
+public class Ship extends Sprite {
+    public Ship(TextureRegion region) {
+        super(region);
+    }
+}
+
+
