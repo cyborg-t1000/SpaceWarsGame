4841b29bb5195ef581fc2a7c08a947218ed534a1
Maxim Nekrasov <kgtu@mail.ru>
2021-02-22 10:54:36 -0500

    Background music

--- core/src/com/kgtu/spacewars/sprite/MainShip.java
+++ core/src/com/kgtu/spacewars/sprite/MainShip.java
@@ -1,6 +1,8 @@
 package com.kgtu.spacewars.sprite;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
@@ -33,6 +35,8 @@ public class MainShip extends Sprite {
     private float reloadInterval;
     private float reloadTimer;
 
+    private Sound sound;
+
     public MainShip(TextureAtlas atlas, BulletPool bulletPool) {
         super(atlas.findRegion("main_ship"), 1, 2, 2);
         this.bulletPool = bulletPool;
@@ -40,6 +44,7 @@ public class MainShip extends Sprite {
         bulletV = new Vector2(0, 0.5f);
         bulletPos = new Vector2();
         reloadInterval = 0.15f;
+        this.sound = Gdx.audio.newSound(Gdx.files.internal("sounds/laser.wav"));
     }
 
     @Override
@@ -164,6 +169,7 @@ public class MainShip extends Sprite {
         Bullet bullet = bulletPool.obtain();
         bulletPos.set(pos.x, pos.y + getHalfHeight());
         bullet.set(this, bulletRegion, bulletPos, bulletV, 0.01f, worldBounds, 1);
+        sound.play();
     }
 
 }
